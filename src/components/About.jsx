738b092209c67397fdf5eb5309c07d6c04c28b5f
Judith Bohmann <judith.bohmann@dci-student.org>
2025-03-06 20:00:29 +0100

    set up Portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,81 @@
 function About() {
   return (
     <>
-      <div>About</div>
+      <div className="aboutContainer">
+        <h2>About</h2>
+        <div className="photo">photo</div>
+
+        <div className="facts">
+          <h3>Facts</h3>
+          <ul>
+            <div className="facts_ul">
+              <li>Name</li>
+              <li>Judith Bohmann</li>
+            </div>
+
+            <div className="facts_ul">
+              <li>Based in</li>
+              <li>Potsdam, Germany</li>
+            </div>
+
+            <div className="facts_ul">
+              <li>Mail</li>
+              <li>ju.bohmann@gmx.de</li>
+            </div>
+
+            <div className="facts_ul">
+              <li>Mobile</li>
+              <li>+49 176 61 89 74 52 </li>
+            </div>
+          </ul>
+        </div>
+
+        <div className="skills">skills</div>
+
+        <div className="quali">
+          <h3>Qualifications</h3>
+          <ul>
+            <li>
+              - 2009 -
+              <br />
+              Bachelore of Arts - Social Work
+              <br />
+              Hochschule Niederrhein
+            </li>
+            <li>
+              - 2024 -
+              <br />
+              Basic Python Training
+              <br />
+              creating simple programmes in python
+            </li>
+            <li>
+              - 2025 -
+              <br />
+              WebDev Training
+              <br />
+              CSS/HTML, Javascript, React and much more ...
+            </li>
+          </ul>
+        </div>
+        <div className="xp">
+          <h2>Professional experience</h2>
+          <h3>Motognosis GmbH</h3>
+          <p>2020 - 2024</p>
+          <p>
+            Implementation of the QM for the medical device 'Amsa' according to
+            ISO 13485
+          </p>
+          <p>
+            Establisment of the IT infrastrusture - as well as data and quality
+            assurance processes for a study of the Charité with 150 patients
+          </p>
+          <p>
+            Development of algorithmic analyses and measurement data analyses in
+            medical studies in the field of motion analysis
+          </p>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,86 @@
 function Portfolio() {
+  const portfolio = [
+    {
+      id: 1,
+      name: "Pokedex",
+      description:
+        "This responsive page intercepts the PokeApi and uses the data it receives to display in a 'Pokedex'. The english name of one of the more than 1000 Pokemon can be entered in the input field. If you press Enter or the 'Search' button, a picture, the stats and the abilities of the Pokemon are displayed. If the Pokemon cannot be found - because it does not exist or a spelling mistake has been made - a NotFound page will appear with a Ghastly, which will randomly appear on the page and disappear again. ",
+      image:
+        "https://raw.githubusercontent.com/You-Did-Bowman/Pokedex/main/src/assets/Pokedex_Search-Pokemon.png",
+      webpageLink: "https://pokedex-dlii.onrender.com/",
+      githubLink: "https://github.com/You-Did-Bowman/Pokedex",
+      technics: ["JavaScript", "CSS", "HTML", "React.js", "Vite.js", "PokeApi"],
+    },
+    {
+      id: 2,
+      name: "To Do List App",
+      description:
+        "This project was a group project at the end of the 'Single Page Application' module in my further training at the DCI. Our group consisted of three people. Our group presents an App of To-Do Tasks with some additional features that are not usually inplemented in mobile versions of such sort of apps. The choice was made on basis of the time frame for creating the code, solving issues of combining data from different branches, proper styling and finally deployment of the project on render.",
+      image:
+        "https://raw.githubusercontent.com/You-Did-Bowman/Project_To-Do-List/refs/heads/main/src/assets/LandingPage-with-tasks.png",
+      webpageLink: "https://project-to-do-list-u8kr.onrender.com/",
+      githubLink: "https://github.com/You-Did-Bowman/Project_To-Do-List",
+      technics: [
+        "JavaScript",
+        "CSS",
+        "HTML",
+        "React.js",
+        "Vite.js",
+        "Router",
+        "Context",
+        "Localstorage",
+      ],
+    },
+    {
+      id: 3,
+      name: "RGB Guessing Game",
+      description:
+        "This game is designed to help you test and improve your knowledge of RGB colour codes. The rules consist of finding the right colour by only being told the RGB colour code. If you guess wrong, the selected tile disappears. However, if you have guessed correctly, all remaining tiles take on the colour you have chosen. There are two difficulty level to choose from.",
+      image:
+        "https://raw.githubusercontent.com/You-Did-Bowman/RGB_Guessing_Game/main/assets/Screenshot_RGB_HARD.png",
+      webpageLink: "https://you-did-bowman.github.io/RGB_Guessing_Game/",
+      githubLink: "https://github.com/You-Did-Bowman/RGB_Guessing_Game",
+      technics: ["JavaScript", "CSS", "HTML"],
+    },
+    ,
+    {
+      id: 4,
+      name: "MASTERMIND CLI-Game ",
+      description: `This was my final project for the “Programming Basics” module at my school. This game only works inside the Terminal. In Mastermind, the coder creates a secret number combination that the decryptor must guess. After each attempt, the decryptor gets feedback with 🔴, 🟡 and 🟢.
+        🔴 = indicates how many numbers do not exist in the solution
+        🟡 = indicates how many numbers appear in the solution but are in the wrong place
+        🟢 = indicates how many numbers are on their right place
+        The decryptor has to find out the combination in as few attempts as possible.`,
+      image:
+        "https://raw.githubusercontent.com/You-Did-Bowman/MASTERMIND_digital/main/images/Screenshot_Mastermind_instructions.png",
+      webpageLink: "",
+      githubLink: "https://github.com/You-Did-Bowman/MASTERMIND_digital",
+      technics: ["JavaScript", "npm chalk", "prompt-sync"],
+    },
+  ];
+
   return (
     <>
-      <div>Portfolio</div>
+      <h2>Portfolio</h2>
+      <div className="portContainer">
+          {portfolio.map((proj) => (
+            <div key={proj.id}>
+              <h3>{proj.name}</h3>
+              <img src={proj.image} alt={proj.name} />
+              <p>{proj.description}</p>
+              <p> Link: <a href={proj.webpageLink} target="_blank">{proj.name}</a></p>
+              <p> <a href={proj.githubLink} target="_blank">Github</a></p>
+
+              <h3>Stack</h3>
+              <ul>
+                {proj.technics.map((el) => (
+                  <li key = {el}>{el}</li>
+                ))}
+              </ul>
+
+              </div>
+          ))}
+      </div>
     </>
   );
 }
